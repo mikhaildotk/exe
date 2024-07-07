7279647029bf64336b3f3d4c358e67802ebefa05
root <kapranov.m@gmail.com>
2024-07-07 10:57:09 +0400

    Task 1 complete

--- homework2/homework2.sh
+++ homework2/homework2.sh
@@ -8,3 +8,15 @@ MKWORKDIR=$(readlink -f ${WORKDIR})
 
 # Проверим/создадим
 test ! -d ${MKWORKDIR} && mkdir ${MKWORKDIR} || echo "${MKWORKDIR} already exists!"
+
+# Переходим в созданную директорию
+cd ${MKWORKDIR} && echo "Current location is ${MKWORKDIR}"
+
+# Создадим file1.txt, file2.txt, file3.txt
+touch file{1..3}.txt
+
+# Добавим в созданные файлы строку "Hello, World!"
+for i in file{1..3}.txt; do echo "Hello, World!" > $i; done
+
+# Выполним пункты 5,6,7 задания #1
+cp -a file1.txt file1_copy.txt && mv file2.txt ../ && rm file3.txt
