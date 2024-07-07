9f5e08eb04598c945f006ca0832078c405cd2727
root <kapranov.m@gmail.com>
2024-07-07 12:17:14 +0400

    Task 2 complete

--- homework2/homework2.sh
+++ homework2/homework2.sh
@@ -6,6 +6,15 @@ WORKDIR="devops_homework"
 # На всякий случай приведем пути к абсолютному виду
 MKWORKDIR=$(readlink -f ${WORKDIR})
 
+# Пользователь
+DEVOPS_USER=devops_user
+# Группа
+DEVOPS_GROUP=devops_group
+
+#---#
+# 1 #
+#---#---------------------------------------------
+
 # Проверим/создадим
 test ! -d ${MKWORKDIR} && mkdir ${MKWORKDIR} || echo "${MKWORKDIR} already exists!"
 
@@ -19,4 +28,34 @@ touch file{1..3}.txt
 for i in file{1..3}.txt; do echo "Hello, World!" > $i; done
 
 # Выполним пункты 5,6,7 задания #1
-cp -a file1.txt file1_copy.txt && mv file2.txt ../ && rm file3.txt
+cp -a file1.txt file1_copy.txt && mv ./file2.txt ../ && rm ./file3.txt
+
+#---#
+# 2 #
+#---#---------------------------------------------
+
+# Для следующих операций, нам нужна эскалация прав
+if [ "$(id -u)" -ne 0 ]; then
+  echo >&2 "Only root!"
+  exit 1
+fi
+
+# Пользователь и группа - проверим/создадим
+getent passwd ${DEVOPS_USER} > /dev/null 2>&1 && echo "User ${DEVOPS_USER} already exist!" || useradd ${DEVOPS_USER}
+getent group ${DEVOPS_GROUP} > /dev/null 2>&1 && echo "Group ${DEVOPS_GROUP} already exist!" || useradd ${DEVOPS_GROUP}
+
+# Пользователя devops_user в группу devops_group
+usermod -aG devops_group devops_user
+
+# Элегантнее так
+# install --mode=664 --owner=devops_user --group=devops_group /dev/null ${MKWORKDIR}/shared_file.txt
+
+# Создадим файл и установим права
+touch ${MKWORKDIR}/shared_file.txt chmod 660 ${MKWORKDIR}/shared_file.txt
+
+# Сменим владельца и группу
+chown devops_user:devops_group ${MKWORKDIR}/shared_file.txt
+
+#---#
+# 3 #
+#---#---------------------------------------------
